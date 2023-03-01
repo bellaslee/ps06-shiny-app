fd791c6453df046f6b53df146e9a0bba55dc47b8
bella <bella@bellalee.com>
2023-03-01 15:38:23 -0800

    Get working plot and table in tab panels

--- app/app.R
+++ app/app.R
@@ -1,68 +1,98 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
+# Shiny web application displaying information on the release year for movies
+# on various streaming platforms. Includes a brief summary, an interactive plot,
+# and an interactive table.
 #
 
+# Load libraries
 library(shiny)
 library(tidyverse)
 
-# NOTE: set your working directory to this folder when you're working on it, don't
-# move the data file.
+# Import data
 data <- read_delim("data/streaming-platform-data.csv")
+View(data)
+names(data)
+
+# List of streaming services in the dataset
 services = c("Netflix", "Hulu", "Prime Video", "Disney+")
 
-# Define UI for application that draws a histogram
+# Define UI for application that shows tabs for a table, histogram, and explanation.
 ui <- fluidPage(
-  
-  titlePanel("Movies by Year for Different Streaming Services"), 
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("service",
-                  "Select streaming service:",
-                  choices = services),
-      sliderInput("year", 
-                  "Select year range:", 
-                  min = min(data$Year), 
-                  max = max(data$Year), 
-                  c(1940, 2002),
-                  sep = "")
-    ),
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Movies by Year", plotOutput("plot"))
+  tabsetPanel(
+    tabPanel(
+      "Visualizations",
+      titlePanel("Movies by Year"),
+      sidebarLayout(
+        sidebarPanel(
+          selectInput("service",
+                      "Select streaming service:",
+                      choices = services),
+          sliderInput("year", 
+                      "Select year range:", 
+                      min = min(data$Year), 
+                      max = max(data$Year), 
+                      c(1940, 2002),
+                      sep = ""),
+          textOutput("description")
+        ),
+        mainPanel(
+          tabsetPanel(
+            tabPanel(
+              "Plot",
+              plotOutput("plot")
+            ),
+            tabPanel(
+              "Table",
+              dataTableOutput("table")
+            )
+          )
+        )
       )
     )
   )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
   
-  # Get information from input
-  service <- reactive({input$service})
-  min_year <- reactive({input$year[1]})
-  max_year <- reactive({input$year[2]})
-  
-  filtered <- data %>% 
-    filter(Year >= min_year & Year <= max_year) %>% 
-    filter(!!as.symbol(service) == 1)
-  
   output$plot <- renderPlot({
-    ggplot(filtered) +
+    
+    # Filter data by minimum year, maximum year, and streaming service.
+    filtered <- data %>%
+      filter(Year >= input$year[1] & Year <= input$year[2]) %>%
+      filter(!!rlang::sym(input$service) == 1)
+    
+    ggplot(filtered, aes(fill = as.factor(Year))) +
       geom_bar(mapping = aes(x = filtered$Year), stat = "count") +
-      labs(title = paste("Movies by Year on", service),
+      labs(title = paste("Movies by Year on", input$service),
            x = "Year",
            y = "Movie count",
            fill = "Year")
   })
   
   output$description <- renderText({
+    filtered <- data %>%
+      filter(Year >= input$year[1] & Year <= input$year[2]) %>%
+      filter(!!rlang::sym(input$service) == 1)
     
+    paste("There are", nrow(filtered), 
+          "movies that were released between", input$year[1], "and", input$year[2], 
+          "on", input$service)
   })
+  
+  output$table <- renderDataTable(
+    data %>% 
+      filter(Year >= input$year[1] & Year <= input$year[2]) %>% 
+      filter(!!rlang::sym(input$service) == 1) %>% 
+      mutate(
+        Netflix = NULL,
+        Hulu = NULL,
+        `Prime Video` = NULL,
+        `Disney+` = NULL,
+        Type = NULL,
+        `...1` = NULL,
+        ID = NULL
+      )
+  )
 }
 
 # Run the application 
